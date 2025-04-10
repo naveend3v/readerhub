2176d6e0e6428d6dc5eca7ccb97c670166e9db9d
naveend3v <naveenselvaraj1997@gmail.com>
2025-04-10 15:00:11 +0530

    initial commit

---
+++ src/main/java/com/naveend3v/readerhub/ReaderhubApplication.java
@@ -0,0 +1,13 @@
+package com.naveend3v.readerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaderhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naveend3v/readerhub/ReaderhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naveend3v.readerhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
